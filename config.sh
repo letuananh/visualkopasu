deb69d303e3f0ee80b2b4fc2b0b6ba7811f39ffd
Le Tuan Anh <tuananh.ke@gmail.com>
2016-06-08 23:44:42 -0400

    Use latest version of CoolISF

---
+++ config.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/sh
+
+function link_folder {
+	FOLDER_PATH=$1
+	SYMLINK_NAME=$2
+	if [ ! -d ${SYMLINK_NAME} ]; then
+		ln -sv ${FOLDER_PATH} ${SYMLINK_NAME}
+	else
+		echo "Folder ${SYMLINK_NAME} exists."
+	fi
+}
+
+function link_file {
+	FOLDER_PATH=$1
+	SYMLINK_NAME=$2
+	if [ ! -f ${SYMLINK_NAME} ]; then
+		ln -sv ${FOLDER_PATH} ${SYMLINK_NAME}
+	else
+		echo "File ${SYMLINK_NAME} exists."
+	fi
+}
+
+cd modules/intsem.fx
+git submodule init && git submodule update
+./config.sh
+cd ../../
+
+link_folder `readlink -f ../pydelphin/delphin` delphin
+link_folder `readlink -f ../beautifulsoup/bs4-python3` bs4
+link_folder `readlink -f ../nltk/nltk` nltk
+
+link_folder `readlink -f ./modules/intsem.fx/chirptext` chirptext
+link_folder `readlink -f ./modules/intsem.fx/lelesk` lelesk
+
+
+link_file `readlink -f ~/workspace/erg/erg.dat` data/erg.dat
+
+git submodule init && git submodule update
