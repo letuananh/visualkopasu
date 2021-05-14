da382e2050c3dc10d118b7bc9456db76125c1b03
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-14 16:45:34 +0800

    add build script

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ find . -name "*.py~" -exec rm -rf {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
 
-tar -zcvf dist/visko_site.tar.gz visko_site manage.py
+tar -zcvf dist/visko.tar.gz visko_site manage.py requirements.txt LICENSE.txt README.md
