3c26a26d5d026a93dc89ab61c8221e1d97a916e1
Le Tuan Anh <tuananh.ke@gmail.com>
2021-05-14 17:25:32 +0800

    build to visko_site

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ find . -name "*.py~" -exec rm -rf {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
 VISKO_VERSION=`python3 -m visko version -s`
-tar -zcvf dist/visko-${VISKO_VERSION}.tar.gz visko_site manage.py requirements.txt LICENSE.txt README.md
+tar -zcvf dist/visko_site-${VISKO_VERSION}.tar.gz visko_site manage.py requirements.txt LICENSE.txt README.md
